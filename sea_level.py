c5c93e5f1ff748b8fc09e252ca78f2b17fa447b6
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 20:54:51 -0500

    Add second line of best fit from the year 2000.

--- sea_level.py
+++ sea_level.py
@@ -13,3 +13,11 @@ res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
 x_pred = pd.Series([i for i in range(1880, 2051)])
 y_pred = res.intercept + res.slope * x_pred
 plt.plot(x_pred, y_pred, 'r')
+
+# Create second line of best fit from 2000
+new_df = df[df['Year'] >= 2000]
+res_new = linregress(new_df['Year'], new_df['CSIRO Adjusted Sea Level'])
+x_pred_new = pd.Series([i for i in range(2000, 2051)])
+y_pred_new = res_new.intercept + res_new.slope * x_pred_new
+plt.plot(x_pred_new, y_pred_new, 'green')
+
