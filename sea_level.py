fcf405058d44d7be753b113cc7c02968e4b5cb3f
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 20:46:55 -0500

    Add first line of best fit to scatter plot.

--- sea_level.py
+++ sea_level.py
@@ -8,3 +8,8 @@ df = pd.read_csv('epa-sea-level.csv')
 # Create scatter plot
 plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
+# Create first line of best fit
+res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+x_pred = pd.Series([i for i in range(1880, 2051)])
+y_pred = res.intercept + res.slope * x_pred
+plt.plot(x_pred, y_pred, 'r')
